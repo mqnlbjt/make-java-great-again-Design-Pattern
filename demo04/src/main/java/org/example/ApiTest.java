3b3dec87e870013a8bb57d1b86673354162262aa
mqnlbjt <790255072@qq.com>
2023-09-23 21:43:06 +0800

    模拟乱序出题

---
+++ demo04/src/main/java/org/example/ApiTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class ApiTest {
+
+    @Test
+    public void test_QuestionBankController() {
+        QuestionBankController questionBankController = new QuestionBankController();
+        System.out.println(questionBankController.createPaper("花花", "1000001921032"));
+        System.out.println(questionBankController.createPaper("豆豆", "1000001921051"));
+        System.out.println(questionBankController.createPaper("大宝", "1000001921987"));
+    }
+
+}
